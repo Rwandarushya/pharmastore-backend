ea4b002d1186731c64004aa8c24e639447cc7445
Marius Robert Rwandarushya <rwandarushyarobert@gmail.com>
2020-12-02 19:37:13 +0200

    modified delete list

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,13 +1,12 @@
 import express from 'express'
 import {createProduct,getAllProducts,getProductById,deleteProductById,updateProductById} from '../controller/product.controller'
-import { verifyAdminToken } from '../middleware/verifyAuthToken';
 const router = express.Router();
 
 router.post('/', createProduct);
 router.get('/:email',getAllProducts);
 router.get('/:id',getProductById);
 router.patch('/:id',updateProductById);
-router.delete('/:id',[verifyAdminToken],deleteProductById);
+router.delete('/:id',deleteProductById);
 
 
   export default router;
